04dbe30f49468742b412f5ef0bf33f2fc9e156d4
Pavel Korotkov <pkorotkov@gmail.com>
2020-07-23 14:29:36 +0300

    Refactor if-blocks in GetBearerToken method

--- auth/bearer-token.go
+++ auth/bearer-token.go
@@ -13,14 +13,15 @@ const bearerTokenContextKey contextKey = "bearer-token"
 
 // GetBearerToken returns a bearer token embedded into a context or error, if any.
 func GetBearerToken(ctx context.Context) (*service.BearerTokenMsg, error) {
-	if bt := ctx.Value(bearerTokenContextKey); bt != nil {
-		v, ok := bt.(*service.BearerTokenMsg)
-		if !ok {
-			return nil, errors.Errorf("extracted unexpected type other than bearer token's: %T", v)
-		}
-		return v, nil
+	bt := ctx.Value(bearerTokenContextKey)
+	if bt == nil {
+		return nil, errors.New("got nil bearer token")
 	}
-	return nil, errors.New("got nil bearer token")
+	v, ok := bt.(*service.BearerTokenMsg)
+	if !ok {
+		return nil, errors.Errorf("extracted unexpected type other than bearer token's: %T", v)
+	}
+	return v, nil
 }
 
 // SetBearerToken return a context with embedded bearer token.
