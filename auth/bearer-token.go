ae66e4de61acb8fa7883b978a1669af57e7e3f84
Pavel Korotkov <pkorotkov@gmail.com>
2020-07-23 11:05:50 +0300

    Add throwing error for GetBearerToken method

--- auth/bearer-token.go
+++ auth/bearer-token.go
@@ -4,18 +4,23 @@ import (
 	"context"
 
 	"github.com/nspcc-dev/neofs-api-go/service"
+	"github.com/pkg/errors"
 )
 
 type contextKey string
 
 const bearerTokenContextKey contextKey = "bearer-token"
 
-// GetBearerToken returns a bearer token embedded into a context.
-func GetBearerToken(ctx context.Context) *service.BearerTokenMsg {
+// GetBearerToken returns a bearer token embedded into a context or error, if any.
+func GetBearerToken(ctx context.Context) (*service.BearerTokenMsg, error) {
 	if bt := ctx.Value(bearerTokenContextKey); bt != nil {
-		return bt.(*service.BearerTokenMsg)
+		v, ok := bt.(*service.BearerTokenMsg)
+		if !ok {
+			return nil, errors.Errorf("extracted unexpected type other than bearer token's: %T", v)
+		}
+		return v, nil
 	}
-	return nil
+	return nil, errors.New("got nil bearer token")
 }
 
 // SetBearerToken return a context with embedded bearer token.
