97dcb943c2722e12204258996144139f2042aa5b
Denis Kirillov <denis@nspcc.ru>
2021-07-01 14:25:16 +0300

    [#94] Fix time format
    Signed-off-by: Denis Kirillov <denis@nspcc.ru>

--- api/handler/get.go
+++ api/handler/get.go
@@ -58,7 +58,7 @@ func writeHeaders(h http.Header, info *layer.ObjectInfo) {
 	if len(info.ContentType) > 0 {
 		h.Set(api.ContentType, info.ContentType)
 	}
-	h.Set(api.LastModified, info.Created.Format(http.TimeFormat))
+	h.Set(api.LastModified, info.Created.Format(time.RFC3339))
 	h.Set(api.ContentLength, strconv.FormatInt(info.Size, 10))
 	h.Set(api.ETag, info.HashSum)
 
