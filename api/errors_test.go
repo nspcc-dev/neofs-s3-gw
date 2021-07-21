c7cf5afd2ff20bbbf9291dc2aa6d7805d882e5c7
Denis Kirillov <denis@nspcc.ru>
2021-07-21 12:05:24 +0300

    [#149] Add benchmarks
    Signed-off-by: Denis Kirillov <denis@nspcc.ru>

---
+++ api/errors_test.go
@@ -0,0 +1,26 @@
+package api
+
+import (
+	"errors"
+	"testing"
+)
+
+func BenchmarkErrCode(b *testing.B) {
+	err := GetAPIError(ErrNoSuchKey)
+
+	for i := 0; i < b.N; i++ {
+		if IsS3Error(err, ErrNoSuchKey) {
+			_ = err
+		}
+	}
+}
+
+func BenchmarkErrorsIs(b *testing.B) {
+	err := GetAPIError(ErrNoSuchKey)
+
+	for i := 0; i < b.N; i++ {
+		if errors.Is(err, GetAPIError(ErrNoSuchKey)) {
+			_ = err
+		}
+	}
+}
