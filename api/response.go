16296490ee4552676c838017f8c7c91ce08dbc44
Alex Vanin <alexey@nspcc.ru>
2021-09-15 12:32:26 +0300

    [#264] Adopt minio code distributed with AGPLv3 license in gateway components
    Signed-off-by: Alex Vanin <alexey@nspcc.ru>

--- api/response.go
+++ api/response.go
@@ -1,11 +1,15 @@
 package api
 
 import (
+	"bytes"
 	"encoding/xml"
 	"fmt"
+	"net/http"
+	"strconv"
 
 	"github.com/google/uuid"
 	"github.com/nspcc-dev/neofs-s3-gw/api/errors"
+	"github.com/nspcc-dev/neofs-s3-gw/internal/version"
 )
 
 type (
@@ -103,6 +107,109 @@ var s3ErrorResponseMap = map[string]string{
 	"InvalidDuration":                   "Duration provided in the request is invalid.",
 	"XAmzContentSHA256Mismatch":         "The provided 'x-amz-content-sha256' header does not match what was computed.",
 	// Add new API errors here.
+}
+
+// WriteErrorResponse writes error headers.
+func WriteErrorResponse(w http.ResponseWriter, reqInfo *ReqInfo, err error) {
+	code := http.StatusInternalServerError
+
+	if e, ok := err.(errors.Error); ok {
+		code = e.HTTPStatusCode
+
+		switch e.Code {
+		case "SlowDown", "XNeoFSServerNotInitialized", "XNeoFSReadQuorum", "XNeoFSWriteQuorum":
+			// Set retry-after header to indicate user-agents to retry request after 120secs.
+			// https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Retry-After
+			w.Header().Set(hdrRetryAfter, "120")
+		case "AccessDenied":
+			// TODO process when the request is from browser and also if browser
+		}
+	}
+
+	// Generate error response.
+	errorResponse := getAPIErrorResponse(reqInfo, err)
+	encodedErrorResponse := EncodeResponse(errorResponse)
+	WriteResponse(w, code, encodedErrorResponse, MimeXML)
+}
+
+// If none of the http routes match respond with appropriate errors.
+func errorResponseHandler(w http.ResponseWriter, r *http.Request) {
+	desc := fmt.Sprintf("Unknown API request at %s", r.URL.Path)
+	WriteErrorResponse(w, GetReqInfo(r.Context()), errors.Error{
+		Code:           "XMinioUnknownAPIRequest",
+		Description:    desc,
+		HTTPStatusCode: http.StatusBadRequest,
+	})
+}
+
+// Write http common headers.
+func setCommonHeaders(w http.ResponseWriter) {
+	w.Header().Set(hdrServerInfo, version.Server)
+	w.Header().Set(hdrAcceptRanges, "bytes")
+
+	// Remove sensitive information
+	removeSensitiveHeaders(w.Header())
+}
+
+// removeSensitiveHeaders removes confidential encryption
+// information - e.g. the SSE-C key - from the HTTP headers.
+// It has the same semantics as RemoveSensitiveEntries.
+func removeSensitiveHeaders(h http.Header) {
+	h.Del(hdrSSECustomerKey)
+	h.Del(hdrSSECopyKey)
+}
+
+// WriteResponse writes given statusCode and response into w (with mType header if set).
+func WriteResponse(w http.ResponseWriter, statusCode int, response []byte, mType mimeType) {
+	setCommonHeaders(w)
+	if mType != MimeNone {
+		w.Header().Set(hdrContentType, string(mType))
+	}
+	w.Header().Set(hdrContentLength, strconv.Itoa(len(response)))
+	w.WriteHeader(statusCode)
+	if response == nil {
+		return
+	}
+
+	_, _ = w.Write(response)
+	if flusher, ok := w.(http.Flusher); ok {
+		flusher.Flush()
+	}
+}
+
+// EncodeResponse encodes the response headers into XML format.
+func EncodeResponse(response interface{}) []byte {
+	var bytesBuffer bytes.Buffer
+	bytesBuffer.WriteString(xml.Header)
+	_ = xml.
+		NewEncoder(&bytesBuffer).
+		Encode(response)
+	return bytesBuffer.Bytes()
+}
+
+// EncodeToResponse encodes the response into ResponseWriter.
+func EncodeToResponse(w http.ResponseWriter, response interface{}) error {
+	w.WriteHeader(http.StatusOK)
+
+	if _, err := w.Write(xmlHeader); err != nil {
+		return err
+	} else if err = xml.NewEncoder(w).Encode(response); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// // WriteSuccessResponseXML writes success headers and response if any,
+// // with content-type set to `application/xml`.
+// func WriteSuccessResponseXML(w http.ResponseWriter, response []byte) {
+// 	WriteResponse(w, http.StatusOK, response, MimeXML)
+// }
+
+// WriteSuccessResponseHeadersOnly writes HTTP (200) OK response with no data
+// to the client.
+func WriteSuccessResponseHeadersOnly(w http.ResponseWriter) {
+	WriteResponse(w, http.StatusOK, nil, MimeNone)
 }
 
 // Error - Returns S3 error string.
