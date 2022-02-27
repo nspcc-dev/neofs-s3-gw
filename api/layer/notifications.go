e6007d5e1553d3cc9797c60554cec5c1d8f3ca68
Angira Kekteeva <kira@nspcc.ru>
2022-02-28 02:50:12 +0300

    [#357] Add event
    Signed-off-by: Angira Kekteeva <kira@nspcc.ru>

--- api/layer/notifications.go
+++ api/layer/notifications.go
@@ -186,3 +186,39 @@ func prepareTestEvent(bktName, requestID, host string) *notifications.TestEvent 
 		HostID:    host,
 	}
 }
+func prepareEvent(eventName string, bkt *data.BucketInfo, obj *data.ObjectInfo, reqInfo *api.ReqInfo) *notifications.Event {
+	return &notifications.Event{
+		Records: []notifications.EventRecord{
+			{
+				EventVersion: notifications.EventVersion21,
+				EventSource:  "neofs:s3",
+				AWSRegion:    "",
+				EventTime:    obj.Created,
+				EventName:    eventName,
+				UserIdentity: notifications.UserIdentity{
+					PrincipalID: obj.Owner.String(),
+				},
+				RequestParameters: notifications.RequestParameters{
+					SourceIPAddress: reqInfo.RemoteHost,
+				},
+				ResponseElements: nil,
+				S3: notifications.S3Entity{
+					SchemaVersion: "1.0",
+					// ConfigurationID is skipped and will be placed later
+					Bucket: notifications.Bucket{
+						Name:          bkt.Name,
+						OwnerIdentity: notifications.UserIdentity{PrincipalID: bkt.Owner.String()},
+						Arn:           "",
+					},
+					Object: notifications.Object{
+						Key:       obj.Name,
+						Size:      obj.Size,
+						VersionID: obj.Version(),
+						ETag:      obj.HashSum,
+						Sequencer: "",
+					},
+				},
+			},
+		},
+	}
+}
