a75a8225e0fdf0848529f37c6a1caf83af04c84d
Denis Kirillov <denis@nspcc.ru>
2022-05-18 16:39:26 +0300

    [#413] Fix sync tree script
    Signed-off-by: Denis Kirillov <denis@nspcc.ru>

--- syncTree.sh
+++ syncTree.sh
@@ -2,13 +2,18 @@
 
 mkdir -p internal/neofs/services/tree 2>/dev/null
 
-REVISION="d4babb0eb89aff316eca067e219c63a0e2ef349b"
+REVISION="1678fe44ed2bdac1c3ae3421d2cc500c2be87a98"
 
 echo "tree service revision ${REVISION}"
 
 FILES=$(curl -s https://github.com/nspcc-dev/neofs-node/tree/${REVISION}/pkg/services/tree | sed -n "s,.*\"/nspcc-dev/neofs-node/blob/${REVISION}/pkg/services/tree/\(.*\.pb\.go\)\".*,\1,p")
 
 for file in $FILES; do
-  echo "sync '$file' in tree service"
+  if [[ $file == *"neofs"* ]]; then
+    echo "skip '$file'"
+    continue
+  else
+    echo "sync '$file' in tree service"
+  fi
   curl -s "https://raw.githubusercontent.com/nspcc-dev/neofs-node/${REVISION}/pkg/services/tree/${file}" -o "./internal/neofs/services/tree/${file}"
 done
