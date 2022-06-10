61525eafff641b8175a94dfce219a6b999017f46
Denis Kirillov <denis@nspcc.ru>
2022-06-10 14:57:41 +0300

    [#524] Optimize listing
    Signed-off-by: Denis Kirillov <denis@nspcc.ru>

--- api/data/tree.go
+++ api/data/tree.go
@@ -19,9 +19,8 @@ type NodeVersion struct {
 // DeleteMarkerInfo is used to save object info if node in the tree service is delete marker.
 // We need this information because the "delete marker" object is no longer stored in NeoFS.
 type DeleteMarkerInfo struct {
-	FilePath string
-	Created  time.Time
-	Owner    user.ID
+	Created time.Time
+	Owner   user.ID
 }
 
 // ExtendedObjectInfo contains additional node info to be able to sort versions by timestamp.
@@ -37,6 +36,7 @@ type BaseNodeVersion struct {
 	ID        uint64
 	OID       oid.ID
 	Timestamp uint64
+	FilePath  string
 }
 
 type ObjectTaggingInfo struct {
