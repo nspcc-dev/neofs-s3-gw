nspcc-dev/neofs-s3-gw

commit 64a38a4ac7b1f5e74c538583c5df0c51cfb61c79

*: Update to SDK 1.0.0-rc.8
Signed-off-by: Evgeny Baydakov <e.bajdakov@gmail.com>

internal/neofs/neofs.go
@@ -18,6 +18,7 @@ import (
 	"github.com/nspcc-dev/neofs-sdk-go/container"
 	"github.com/nspcc-dev/neofs-sdk-go/container/acl"
 	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
+	neofsecdsa "github.com/nspcc-dev/neofs-sdk-go/crypto/ecdsa"
 	"github.com/nspcc-dev/neofs-sdk-go/eacl"
 	"github.com/nspcc-dev/neofs-sdk-go/object"
 	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
@@ -274,7 +275,7 @@ func (x *NeoFS) CreateObject(ctx context.Context, prm layer.PrmObjectCreate) (oi
 	if prm.BearerToken != nil {
 		prmPut.UseBearer(*prm.BearerToken)
 	} else {
-		prmPut.UseKey(prm.PrivateKey)
+		prmPut.UseSigner(neofsecdsa.SignerRFC6979(*prm.PrivateKey))
 	}
 
 	idObj, err := x.pool.PutObject(ctx, prmPut)
@@ -318,7 +319,7 @@ func (x *NeoFS) ReadObject(ctx context.Context, prm layer.PrmObjectRead) (*layer
 	if prm.BearerToken != nil {
 		prmGet.UseBearer(*prm.BearerToken)
 	} else {
-		prmGet.UseKey(prm.PrivateKey)
+		prmGet.UseSigner(neofsecdsa.SignerRFC6979(*prm.PrivateKey))
 	}
 
 	if prm.WithHeader {
@@ -352,7 +353,7 @@ func (x *NeoFS) ReadObject(ctx context.Context, prm layer.PrmObjectRead) (*layer
 		if prm.BearerToken != nil {
 			prmHead.UseBearer(*prm.BearerToken)
 		} else {
-			prmHead.UseKey(prm.PrivateKey)
+			prmHead.UseSigner(neofsecdsa.SignerRFC6979(*prm.PrivateKey))
 		}
 
 		hdr, err := x.pool.HeadObject(ctx, prmHead)
@@ -390,7 +391,7 @@ func (x *NeoFS) ReadObject(ctx context.Context, prm layer.PrmObjectRead) (*layer
 	if prm.BearerToken != nil {
 		prmRange.UseBearer(*prm.BearerToken)
 	} else {
-		prmRange.UseKey(prm.PrivateKey)
+		prmRange.UseSigner(neofsecdsa.SignerRFC6979(*prm.PrivateKey))
 	}
 
 	res, err := x.pool.ObjectRange(ctx, prmRange)
@@ -419,7 +420,7 @@ func (x *NeoFS) DeleteObject(ctx context.Context, prm layer.PrmObjectDelete) err
 	if prm.BearerToken != nil {
 		prmDelete.UseBearer(*prm.BearerToken)
 	} else {
-		prmDelete.UseKey(prm.PrivateKey)
+		prmDelete.UseSigner(neofsecdsa.SignerRFC6979(*prm.PrivateKey))
 	}
 
 	err := x.pool.DeleteObject(ctx, prmDelete)

api/cache/objects_test.go
@@ -20,7 +20,7 @@ func getTestConfig() *Config {
 }
 
 func TestCache(t *testing.T) {
-	obj := objecttest.Object()
+	obj := objecttest.Object(t)
 	objID, _ := obj.ID()
 	cnrID, _ := obj.ContainerID()
 

api/handler/acl_test.go
@@ -18,6 +18,7 @@ import (
 	"github.com/nspcc-dev/neofs-s3-gw/api/data"
 	"github.com/nspcc-dev/neofs-s3-gw/creds/accessbox"
 	"github.com/nspcc-dev/neofs-sdk-go/bearer"
+	neofsecdsa "github.com/nspcc-dev/neofs-sdk-go/crypto/ecdsa"
 	"github.com/nspcc-dev/neofs-sdk-go/eacl"
 	"github.com/nspcc-dev/neofs-sdk-go/object"
 	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
@@ -1393,7 +1394,7 @@ func createAccessBox(t *testing.T) (*accessbox.Box, *keys.PrivateKey) {
 	require.NoError(t, err)
 
 	var bearerToken bearer.Token
-	err = bearerToken.Sign(key.PrivateKey)
+	err = bearerToken.Sign(neofsecdsa.SignerRFC6979(key.PrivateKey))
 	require.NoError(t, err)
 
 	tok := new(session.Container)

api/handler/handlers_test.go
@@ -19,6 +19,7 @@ import (
 	"github.com/nspcc-dev/neofs-s3-gw/api/layer"
 	"github.com/nspcc-dev/neofs-s3-gw/api/resolver"
 	cid "github.com/nspcc-dev/neofs-sdk-go/container/id"
+	neofsecdsa "github.com/nspcc-dev/neofs-sdk-go/crypto/ecdsa"
 	"github.com/nspcc-dev/neofs-sdk-go/netmap"
 	"github.com/nspcc-dev/neofs-sdk-go/object"
 	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
@@ -76,7 +77,7 @@ func prepareHandlerContext(t *testing.T) *handlerContext {
 	})
 
 	var owner user.ID
-	user.IDFromKey(&owner, key.PrivateKey.PublicKey)
+	require.NoError(t, user.IDFromSigner(&owner, neofsecdsa.SignerRFC6979(key.PrivateKey)))
 
 	layerCfg := &layer.Config{
 		Caches:      layer.DefaultCachesConfigs(zap.NewExample()),

api/handler/head_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/nspcc-dev/neofs-s3-gw/api"
 	"github.com/nspcc-dev/neofs-s3-gw/creds/accessbox"
 	"github.com/nspcc-dev/neofs-sdk-go/bearer"
+	neofsecdsa "github.com/nspcc-dev/neofs-sdk-go/crypto/ecdsa"
 	"github.com/nspcc-dev/neofs-sdk-go/eacl"
 	"github.com/stretchr/testify/require"
 )
@@ -95,7 +96,7 @@ func newTestAccessBox(t *testing.T, key *keys.PrivateKey) *accessbox.Box {
 
 	var btoken bearer.Token
 	btoken.SetEACLTable(*eacl.NewTable())
-	err = btoken.Sign(key.PrivateKey)
+	err = btoken.Sign(neofsecdsa.SignerRFC6979(key.PrivateKey))
 	require.NoError(t, err)
 
 	return &accessbox.Box{

api/layer/layer.go
@@ -2,7 +2,6 @@ package layer
 
 import (
 	"context"
-	"crypto/ecdsa"
 	"crypto/rand"
 	"fmt"
 	"io"
@@ -322,7 +321,9 @@ func (n *layer) Owner(ctx context.Context) user.ID {
 	}
 
 	var ownerID user.ID
-	user.IDFromKey(&ownerID, (ecdsa.PublicKey)(*n.EphemeralKey()))
+	if err := user.IDFromKey(&ownerID, n.EphemeralKey().Bytes()); err != nil {
+		panic(fmt.Errorf("id from key: %w", err))
+	}
 
 	return ownerID
 }

api/layer/versioning_test.go
@@ -10,6 +10,7 @@ import (
 	"github.com/nspcc-dev/neofs-s3-gw/api/data"
 	"github.com/nspcc-dev/neofs-s3-gw/creds/accessbox"
 	bearertest "github.com/nspcc-dev/neofs-sdk-go/bearer/test"
+	neofsecdsa "github.com/nspcc-dev/neofs-sdk-go/crypto/ecdsa"
 	"github.com/nspcc-dev/neofs-sdk-go/object"
 	oid "github.com/nspcc-dev/neofs-sdk-go/object/id"
 	"github.com/nspcc-dev/neofs-sdk-go/user"
@@ -137,8 +138,8 @@ func prepareContext(t *testing.T, cachesConfig ...*CachesConfig) *testContext {
 	key, err := keys.NewPrivateKey()
 	require.NoError(t, err)
 
-	bearerToken := bearertest.Token()
-	require.NoError(t, bearerToken.Sign(key.PrivateKey))
+	bearerToken := bearertest.Token(t)
+	require.NoError(t, bearerToken.Sign(neofsecdsa.SignerRFC6979(key.PrivateKey)))
 
 	ctx := context.WithValue(context.Background(), api.BoxData, &accessbox.Box{
 		Gate: &accessbox.GateData{
@@ -160,7 +161,7 @@ func prepareContext(t *testing.T, cachesConfig ...*CachesConfig) *testContext {
 	}
 
 	var owner user.ID
-	user.IDFromKey(&owner, key.PrivateKey.PublicKey)
+	require.NoError(t, user.IDFromSigner(&owner, neofsecdsa.SignerRFC6979(key.PrivateKey)))
 
 	layerCfg := &Config{
 		Caches:      config,

authmate/authmate.go
@@ -2,7 +2,6 @@ package authmate
 
 import (
 	"context"
-	"crypto/ecdsa"
 	"encoding/hex"
 	"encoding/json"
 	"errors"
@@ -231,7 +230,9 @@ func (a *Agent) IssueSecret(ctx context.Context, w io.Writer, options *IssueSecr
 	box.ContainerPolicy = policies
 
 	var idOwner user.ID
-	user.IDFromKey(&idOwner, options.NeoFSKey.PrivateKey.PublicKey)
+	if err = user.IDFromSigner(&idOwner, neofsecdsa.SignerRFC6979(options.NeoFSKey.PrivateKey)); err != nil {
+		return fmt.Errorf("id from signer: %w", err)
+	}
 
 	a.log.Info("check container or create", zap.Stringer("cid", options.Container.ID),
 		zap.String("friendly_name", options.Container.FriendlyName),
@@ -346,7 +347,9 @@ func restrictedRecords() (records []*eacl.Record) {
 
 func buildBearerToken(key *keys.PrivateKey, table *eacl.Table, lifetime lifetimeOptions, gateKey *keys.PublicKey) (*bearer.Token, error) {
 	var ownerID user.ID
-	user.IDFromKey(&ownerID, (ecdsa.PublicKey)(*gateKey))
+	if err := user.IDFromKey(&ownerID, gateKey.Bytes()); err != nil {
+		return nil, fmt.Errorf("id from key: %w", err)
+	}
 
 	var bearerToken bearer.Token
 	bearerToken.SetEACLTable(*table)
@@ -355,7 +358,7 @@ func buildBearerToken(key *keys.PrivateKey, table *eacl.Table, lifetime lifetime
 	bearerToken.SetIat(lifetime.Iat)
 	bearerToken.SetNbf(lifetime.Iat)
 
-	err := bearerToken.Sign(key.PrivateKey)
+	err := bearerToken.Sign(neofsecdsa.SignerRFC6979(key.PrivateKey))
 	if err != nil {
 		return nil, fmt.Errorf("sign bearer token: %w", err)
 	}
@@ -387,7 +390,7 @@ func buildSessionToken(key *keys.PrivateKey, lifetime lifetimeOptions, ctx sessi
 	tok.SetNbf(lifetime.Iat)
 	tok.SetExp(lifetime.Exp)
 
-	return tok, tok.Sign(key.PrivateKey)
+	return tok, tok.Sign(neofsecdsa.SignerRFC6979(key.PrivateKey))
 }
 
 func buildSessionTokens(key *keys.PrivateKey, lifetime lifetimeOptions, ctxs []sessionTokenContext, gatesKeys []*keys.PublicKey) ([][]*session.Container, error) {

commit ebe412fbbd40505755a6f7e863d8f71ccd1ef432

neofs: don't panic on missing signer key for request, fix #784
It's always nil here and it's OK because there is a key in the pool. Broken
by 64a38a4ac7b1f5e74c538583c5df0c51cfb61c79.

Signed-off-by: Roman Khimov <roman@nspcc.ru>

internal/neofs/neofs.go
@@ -262,7 +262,7 @@ func (x *NeoFS) CreateObject(ctx context.Context, prm layer.PrmObjectCreate) (oi
 
 	if prm.BearerToken != nil {
 		prmPut.UseBearer(*prm.BearerToken)
-	} else {
+	} else if prm.PrivateKey != nil {
 		prmPut.UseSigner(neofsecdsa.SignerRFC6979(*prm.PrivateKey))
 	}
 
@@ -301,7 +301,7 @@ func (x *NeoFS) ReadObject(ctx context.Context, prm layer.PrmObjectRead) (*layer
 
 	if prm.BearerToken != nil {
 		prmGet.UseBearer(*prm.BearerToken)
-	} else {
+	} else if prm.PrivateKey != nil {
 		prmGet.UseSigner(neofsecdsa.SignerRFC6979(*prm.PrivateKey))
 	}
 
@@ -334,7 +334,7 @@ func (x *NeoFS) ReadObject(ctx context.Context, prm layer.PrmObjectRead) (*layer
 
 		if prm.BearerToken != nil {
 			prmHead.UseBearer(*prm.BearerToken)
-		} else {
+		} else if prm.PrivateKey != nil {
 			prmHead.UseSigner(neofsecdsa.SignerRFC6979(*prm.PrivateKey))
 		}
 
@@ -369,7 +369,7 @@ func (x *NeoFS) ReadObject(ctx context.Context, prm layer.PrmObjectRead) (*layer
 
 	if prm.BearerToken != nil {
 		prmRange.UseBearer(*prm.BearerToken)
-	} else {
+	} else if prm.PrivateKey != nil {
 		prmRange.UseSigner(neofsecdsa.SignerRFC6979(*prm.PrivateKey))
 	}
 
@@ -393,7 +393,7 @@ func (x *NeoFS) DeleteObject(ctx context.Context, prm layer.PrmObjectDelete) err
 
 	if prm.BearerToken != nil {
 		prmDelete.UseBearer(*prm.BearerToken)
-	} else {
+	} else if prm.PrivateKey != nil {
 		prmDelete.UseSigner(neofsecdsa.SignerRFC6979(*prm.PrivateKey))
 	}
 