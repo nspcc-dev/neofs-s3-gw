95b3304e1e4abc449cacb43c125094e2545409e9
Evgenii Baidakov <evgenii@nspcc.io>
2025-02-21 10:55:06 +0400

    handler: Remove obsolete tests
    Continuation token is not a OID any more.
    
    Signed-off-by: Evgenii Baidakov <evgenii@nspcc.io>

--- api/handler/object_list_test.go
+++ api/handler/object_list_test.go
@@ -20,24 +20,6 @@ func TestParseContinuationToken(t *testing.T) {
 		_, err = parseContinuationToken(queryValues)
 		require.Error(t, err)
 	})
-
-	t.Run("invalid not empty token", func(t *testing.T) {
-		var queryValues = map[string][]string{
-			"continuation-token": {"asd"},
-		}
-		_, err = parseContinuationToken(queryValues)
-		require.Error(t, err)
-	})
-
-	t.Run("valid token", func(t *testing.T) {
-		tokenStr := "75BTT5Z9o79XuKdUeGqvQbqDnxu6qWcR5EhxW8BXFf8t"
-		var queryValues = map[string][]string{
-			"continuation-token": {tokenStr},
-		}
-		token, err := parseContinuationToken(queryValues)
-		require.NoError(t, err)
-		require.Equal(t, tokenStr, token)
-	})
 }
 
 func TestListObjectNullVersions(t *testing.T) {
